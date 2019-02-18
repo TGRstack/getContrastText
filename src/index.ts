2f1de35df4d2aad9df452c15aca46e499b876eda
Florian Mettetal <florian.mettetal@gmail.com>
2019-02-18 14:41:35 -0800

    :sparkles: ($src) Export helper functions

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@
 
 import * as warning from 'warning';
 
+let logWarnings = true
+
 export default function getContrastText({
   background,
   contrastThreshold,
@@ -17,77 +19,9 @@ export default function getContrastText({
   light: {text: {primary: string}},
   warnings?: boolean
 }) {
-  function getContrastRatio(foreground: string, background: string) {
-    warning(
-      foreground,
-      `Material-UI: missing foreground argument in getContrastRatio(${foreground}, ${background}).`,
-    );
-    warning(
-      background,
-      `Material-UI: missing background argument in getContrastRatio(${foreground}, ${background}).`,
-    );
+  logWarnings = warnings
 
-    const lumA = getLuminance(foreground);
-    const lumB = getLuminance(background);
-    return (Math.max(lumA, lumB) + 0.05) / (Math.min(lumA, lumB) + 0.05);
-  }
-
-  function getLuminance(color: string) {
-    warning(color, `Material-UI: missing color argument in getLuminance(${color}).`);
-
-    const decomposedColor = decomposeColor(color);
-
-    if (decomposedColor.type.indexOf('rgb') !== -1) {
-      const rgb = decomposedColor.values.map(val => {
-        val /= 255; // normalized
-        return val <= 0.03928 ? val / 12.92 : ((val + 0.055) / 1.055) ** 2.4;
-      });
-      // Truncate at 3 digits
-      return Number((0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2]).toFixed(3));
-    }
-
-    // else if (decomposedColor.type.indexOf('hsl') !== -1)
-    return decomposedColor.values[2] / 100;
-  }
-
-  function decomposeColor(color: string): {type: string, values: number[]} {
-    if (color.charAt(0) === '#') {
-      return decomposeColor(convertHexToRGB(color));
-    }
-
-    const marker = color.indexOf('(');
-    const type = color.substring(0, marker);
-    const strValues = color.substring(marker + 1, color.length - 1).split(',');
-    const values = strValues.map(value => parseFloat(value));
-
-    if (warnings) {
-      if (['rgb', 'rgba', 'hsl', 'hsla'].indexOf(type) === -1) {
-        throw new Error(
-          [
-            `Material-UI: unsupported \`${color}\` color.`,
-            'We support the following formats: #nnn, #nnnnnn, rgb(), rgba(), hsl(), hsla().',
-          ].join('\n'),
-        );
-      }
-    }
-
-    return { type, values };
-  }
-
-  function convertHexToRGB(color: string) {
-    color = color.substr(1);
-
-    const re = new RegExp(`.{1,${color.length / 3}}`, 'g');
-    let colors = color.match(re);
-
-    if (colors && colors[0].length === 1) {
-      colors = colors.map(n => n + n);
-    }
-
-    return colors ? `rgb(${colors.map(n => parseInt(n, 16)).join(', ')})` : '';
-  }
-
-  warning(
+  logWarnings && warning(
     background,
     `Material-UI: missing background argument in getContrastText(${background}).`,
   );
@@ -97,7 +31,7 @@ export default function getContrastText({
       ? dark.text.primary
       : light.text.primary;
 
-  if (warnings) {
+  if (logWarnings) {
     const contrast = getContrastRatio(background, contrastText);
     warning(
       contrast >= 3,
@@ -110,4 +44,76 @@ export default function getContrastText({
   }
 
   return contrastText;
+}
+
+export function decomposeColor(color: string): {type: string, values: number[]} {
+  if (color.charAt(0) === '#') {
+    return decomposeColor(convertHexToRGB(color));
+  }
+
+  const marker = color.indexOf('(');
+  const type = color.substring(0, marker);
+  const strValues = color.substring(marker + 1, color.length - 1).split(',');
+  const values = strValues.map(value => parseFloat(value));
+
+  if (logWarnings) {
+    if (['rgb', 'rgba', 'hsl', 'hsla'].indexOf(type) === -1) {
+      throw new Error(
+        [
+          `Material-UI: unsupported \`${color}\` color.`,
+          'We support the following formats: #nnn, #nnnnnn, rgb(), rgba(), hsl(), hsla().',
+        ].join('\n'),
+      );
+    }
+  }
+
+  return { type, values };
+}
+
+export function convertHexToRGB(color: string) {
+  color = color.substr(1);
+
+  const re = new RegExp(`.{1,${color.length / 3}}`, 'g');
+  let colors = color.match(re);
+
+  if (colors && colors[0].length === 1) {
+    colors = colors.map(n => n + n);
+  }
+
+  return colors ? `rgb(${colors.map(n => parseInt(n, 16)).join(', ')})` : '';
+}
+
+export function getContrastRatio(foreground: string, background: string) {
+  if(logWarnings) {
+    warning(
+      foreground,
+      `Material-UI: missing foreground argument in getContrastRatio(${foreground}, ${background}).`,
+    );
+    warning(
+      background,
+      `Material-UI: missing background argument in getContrastRatio(${foreground}, ${background}).`,
+    );
+  }
+
+  const lumA = getLuminance(foreground);
+  const lumB = getLuminance(background);
+  return (Math.max(lumA, lumB) + 0.05) / (Math.min(lumA, lumB) + 0.05);
+}
+
+export function getLuminance(color: string) {
+  logWarnings && warning(color, `Material-UI: missing color argument in getLuminance(${color}).`);
+
+  const decomposedColor = decomposeColor(color);
+
+  if (decomposedColor.type.indexOf('rgb') !== -1) {
+    const rgb = decomposedColor.values.map(val => {
+      val /= 255; // normalized
+      return val <= 0.03928 ? val / 12.92 : ((val + 0.055) / 1.055) ** 2.4;
+    });
+    // Truncate at 3 digits
+    return Number((0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2]).toFixed(3));
+  }
+
+  // else if (decomposedColor.type.indexOf('hsl') !== -1)
+  return decomposedColor.values[2] / 100;
 }
